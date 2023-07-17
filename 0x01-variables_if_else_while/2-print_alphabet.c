b387a8226c884d8822784575b5cecb612db67209
bernabasalx <bernabasalx@gmail.com>
2023-07-17 06:26:21 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+* main - Entry point
+* Description: 'putchar'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alph = a;
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar("\n");
+	return (0);
+}
