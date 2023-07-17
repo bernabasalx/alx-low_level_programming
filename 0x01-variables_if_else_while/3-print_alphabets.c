d49aef10418a546321c68ed44668d5d9cc1f07a0
bernabasalx <bernabasalx@gmail.com>
2023-07-17 06:41:19 -0700

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+* main - Entry point
+* Description: 'putchar'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+        char alph1 = 'a';
+	char alph2 = 'A';
+
+        while (alph1 <= 'z')
+        {
+                putchar(alph1);
+                alph1++;
+        }
+	while (alpha2 <= 'Z')
+	{
+		putchar(alph2);
+		alpha2++;
+	}
+        putchar('\n');
+        return (0);
+}
+
