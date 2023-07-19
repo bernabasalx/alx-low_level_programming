b346b4de43852a868622f48dee936f03f289e974
bernabasalx <bernabasalx@gmail.com>
2023-07-19 00:15:50 -0700

    cisfun

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+
+	/**
+	 * main – Entry point
+	 * Description: cisfun
+	 * Return: Always 0.
+	 */
+	int add(int a, int b)
+	{
+		int c;
+
+
+		c = a + b;
+
+
+		return (c);
+	}
